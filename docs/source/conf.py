e5bf6f8a596dcdbd070b22b92ff1a51b1a3b0655
neosrc <108533354+neosrc1@users.noreply.github.com>
2024-08-11 01:23:01 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,6 +30,10 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    'logo_only': False
+}
+html_logo = 'cc trim.png'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
